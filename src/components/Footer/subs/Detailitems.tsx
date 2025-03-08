a3200fbcb2e76329d0aa24d5554b9bbf53d42f1b
Selander1998 <selanderantonsson@gmail.com>
2025-03-08 07:58:56 -0500

    Added attempt of dynamically creating the details menu for footer

--- src/components/Footer/subs/Detailitems.tsx
+++ src/components/Footer/subs/Detailitems.tsx
@@ -1,34 +1,71 @@
+import React from "react";
+
+interface DetailsMenuItemProps {
+	open: boolean;
+	title: string;
+	subValues: string[];
+	styling: string;
+}
+
+const CreateDetailsMenuItem: React.FC<DetailsMenuItemProps> = ({
+	open,
+	title,
+	subValues,
+	styling,
+}) => {
+	return (
+		<details open={open}>
+			<summary className={`list-none border-2 border-r-0 border-l-0 ${styling}`}>{title}</summary>
+			{subValues.map((value, index) => (
+				<div className="flex flex-col m-1" key={index}>
+					<span>{value}</span>
+				</div>
+			))}
+		</details>
+	);
+};
+
+interface MenuItem {
+	open: boolean;
+	title: string;
+	subValues: string[];
+	styling: string;
+}
+
+const MenuItems: MenuItem[] = [
+	{
+		open: true,
+		title: "Shopping",
+		subValues: ["Vinterjackor", "Pufferjackor", "Kappa", "Trenchcoats"],
+		styling: "",
+	},
+	{
+		open: false,
+		title: "Mina Sidor",
+		subValues: ["Mina ordrar", "Mitt konto"],
+		styling: "border-b-0 border-t-0",
+	},
+	{
+		open: false,
+		title: "Kundtjänst",
+		subValues: ["Returpolicy", "Integritetspolicy"],
+		styling: "border-b-0",
+	},
+];
+
 const DetailsItems = () => {
 	return (
 		<section className="sm:hidden">
-			<details open>
-				<summary className="list-none border-2 border-l-0 border-r-0">Shopping</summary>
-				<div className="footer-details-span">
-					<span>Vinterjackor</span>
-					<span>Pufferjackor</span>
-					<span>Kappa</span>
-					<span>Trenchcoats</span>
-				</div>
-			</details>
-			<details>
-				<summary className="list-none border-2 border-l-0 border-r-0 border-t-0 border-b-0">
-					Mina Sidor
-				</summary>
-				<div className="footer-details-span">
-					<span>Mina ordrar</span>
-					<span>Mitt konto</span>
-				</div>
-			</details>
-			<details>
-				<summary className="list-none border-2 border-b-0 border-l-0 border-r-0">
-					Kundtjänst
-				</summary>
-				<div className="footer-details-span">
-					<span>Returpolicy</span>
-					<span>Integritetspolicy</span>
-				</div>
-			</details>
-			<p className="text-center border-2 pt-2 pb-2 sm:mb-8">&copy;Freaky Fashion</p>
+			{MenuItems.map((item, index) => (
+				<CreateDetailsMenuItem
+					key={`${item.title}-${index}`}
+					open={item.open}
+					title={item.title}
+					subValues={item.subValues}
+					styling={item.styling}
+				/>
+			))}
+			<p className="text-center border-2 pt-2 pb-2 sm:mb-8">{"\u00A9"} Freaky Fashion</p>
 		</section>
 	);
 };
