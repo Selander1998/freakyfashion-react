02c2f73af45f88beb28c567e48ce72876d3246d4
Selander1998 <selanderantonsson@gmail.com>
2025-03-26 17:07:53 -0400

    Add styling to article page

--- src/views/ProductDetailsPage/subs/ProductCard.tsx
+++ src/views/ProductDetailsPage/subs/ProductCard.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface ProductCardProps {
 	article: {
-		title: string;
+		name: string;
 		imageUrl: string;
 		brand: string;
 		description: string;
@@ -16,24 +16,16 @@ export const ProductCard = ({ article }: ProductCardProps) => {
 	return (
 		<div className="flex flex-col p-4 sm:flex-row sm:gap-4">
 			<div className="relative flex flex-col sm:min-w-2/5 lg:min-w-1/2">
-				<img src={article.imageUrl} alt={article.title} className="w-full" />
-				<FontAwesomeIcon icon={faHeart} className="text-2xl" />
+				<img src={article.imageUrl} alt={article.name} className="w-full" />
+				<FontAwesomeIcon icon={faHeart} className="text-3xl absolute right-5 bottom-5" />
+			</div>
+			<div className="flex flex-col gap-3 p-4">
+				<p className="text-2xl">{article.name}</p>
+				<p>{article.brand}</p>
+				<p>{article.description || "Missing description"}</p>
+				<span className="pt-2">{article.price}kr</span>
+				<button className="shadow-2xl p-2 font-bold border-2">Lägg i varukorg</button>
 			</div>
 		</div>
 	);
 };
-
-{
-	/*
-<main>
-    <div class="details-info-container">
-        <h3><%= article.title %></h3>
-        <p class="manufacturer"><%= article.brand %></p>
-        <b class="description"><%= article.description %></b>
-        <span><%= article.price %></span>
-        <button>Lägg i varukorg</button>
-    </div>
-</main>
-
-*/
-}
